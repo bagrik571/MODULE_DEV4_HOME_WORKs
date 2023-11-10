2e506feaf009c8ed428736d6afb3460bf66d9ad0
Vitalii <106606413+bagrik571@users.noreply.github.com>
2023-11-10 19:09:23 +0200

    Delete MODUL8_HOME_WORK directory

--- MODUL8_HOME_WORK/src/ShapePrinter.java
+++
@@ -1,6 +0,0 @@
-public interface ShapePrinter {
-    public default void  print(Shape shape){
-        System.out.println(shape.getName());
-    }
-    public void print(Shape shape, ShapeCharacteristics[] characteristics);
-}
